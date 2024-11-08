102027f89c7cb28b5303eb5c7fb950a0ab280bd4
icedragon500 <493098698@qq.com>
2024-11-08 09:45:46 +0800

    201_PostEditChangeProperties
    当我们更改BP_ProjectileBullet中的初始速度Initial Speed时，我们的ProjectileMovementComponent中的初始速度没有更新，我们需要使用virtual void PostEditChangeProperty(struct FPropertyChangedEvent& Event) override 方法，来对两个属性值进行关联

--- Source/Blaster/Weapon/ProjectileBullet.h
+++ Source/Blaster/Weapon/ProjectileBullet.h
@@ -19,11 +19,21 @@ class BLASTER_API AProjectileBullet : public AProjectile
 public:
 
 	AProjectileBullet();
+
+	/**
+	 * #if WITH_EDITOR 和 #endif 是预处理器指令，用于判断是否在编辑器环境下编译代码。
+	 * WITH_EDITOR 是一个宏定义，通常在使用 Unreal Editor 编译项目时会被定义为 true。
+	 * 如果不是在编辑器环境下（例如，在游戏运行时），则不会编译这两行之间的代码。
+	 */
+#if WITH_EDITOR	
+	virtual void PostEditChangeProperty(struct FPropertyChangedEvent& Event) override;
+#endif
 	
+protected:
 	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit) override;
 	virtual void BeginPlay() override;
 
-protected:
+
 
 private:
 	
